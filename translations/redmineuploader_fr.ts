6d2a2f3c33bffc3d53260e3794ee6f7ab4487060
Kervala <983521+kervala@users.noreply.github.com>
2015-01-20 19:02:16 +0100

    Changed: French translations

--- translations/redmineuploader_fr.ts
+++ translations/redmineuploader_fr.ts
@@ -2,10 +2,105 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="fr_FR" sourcelanguage="en_US">
 <context>
-    <name>CommandLine</name>
+    <name>Redmine</name>
     <message>
-        <source>Upload done.</source>
-        <translation type="unfinished"></translation>
+        <source>Login to %1 with username %2</source>
+        <translation>Connexion à %1 with le nom d&apos;utilisateur %2</translation>
+    </message>
+    <message>
+        <source>Unable to read file %1</source>
+        <translation>Impossible de lire le fichier %1</translation>
+    </message>
+    <message>
+        <source>Uploading %1</source>
+        <translation>Téléversement en cours de %1</translation>
+    </message>
+    <message>
+        <source>Found authenticity token %1</source>
+        <translation>Détection du jeton d&apos;authenticité %1</translation>
+    </message>
+    <message>
+        <source>Found version ID %1</source>
+        <translation>Détection de l&apos;ID de version %1</translation>
+    </message>
+    <message>
+        <source>Unable to find authenticity token</source>
+        <translation>Impossible de trouver le jeton d&apos;authenticité</translation>
+    </message>
+    <message>
+        <source>Unable to find version ID</source>
+        <translation>Impossible de trouver l&apos;ID de version</translation>
+    </message>
+    <message>
+        <source>No files to upload</source>
+        <translation>Aucun fichier à téléverser</translation>
+    </message>
+    <message>
+        <source>Unknown URL: %1</source>
+        <translation>URL inconnue : %1</translation>
+    </message>
+    <message>
+        <source>All files successfully uploaded</source>
+        <translation>Tous les fichiers ont été téléversés avec succès</translation>
+    </message>
+    <message>
+        <source>Unknown URL: %1 or redirection: %2</source>
+        <translation>URL inconnue: %1 ou redirection: %2</translation>
+    </message>
+    <message>
+        <source>Error %1: %2</source>
+        <translation>Erreur %1 : %2</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Redmine root URL</source>
+        <translation>URL racine de Redmine</translation>
+    </message>
+    <message>
+        <source>&lt;root&gt;</source>
+        <translation>&lt;racine&gt;</translation>
+    </message>
+    <message>
+        <source>Redmine project identifier</source>
+        <translation>Identifiant du project Redmine</translation>
+    </message>
+    <message>
+        <source>&lt;project&gt;</source>
+        <translation>&lt;projet&gt;</translation>
+    </message>
+    <message>
+        <source>Project version</source>
+        <translation>Version du projet</translation>
+    </message>
+    <message>
+        <source>&lt;version&gt;</source>
+        <translation>&lt;version&gt;</translation>
+    </message>
+    <message>
+        <source>Redmine username</source>
+        <translation>Nom d&apos;utilisateur de Redmine</translation>
+    </message>
+    <message>
+        <source>&lt;username&gt;</source>
+        <translation>&lt;utilisateur&gt;</translation>
+    </message>
+    <message>
+        <source>Redmine password</source>
+        <translation>Mot de passe de Redmine</translation>
+    </message>
+    <message>
+        <source>&lt;password&gt;</source>
+        <translation>&lt;mot de passe&gt;</translation>
+    </message>
+    <message>
+        <source>Files to upload</source>
+        <translation>Fichiers à téléverser</translation>
+    </message>
+    <message>
+        <source>[filenames...]</source>
+        <translation>[fichiers...]</translation>
     </message>
 </context>
 </TS>
